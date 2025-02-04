f9b1d414ea0609e00a831410e6b5c8972bedfcca
sifupy <sifeddine.addar@gmail.com>
2025-02-04 17:15:54 +0100

    v1.9.6

--- sherlock/settings.py
+++ sherlock/settings.py
@@ -49,7 +49,6 @@ INTERNEL_IPS=['127.0.0.1',]
 NPM_BIN_PATH = 'C:/Program Files/nodejs/npm.cmd'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -129,7 +128,6 @@ STATIC_URL = 'static/'
 STATIC_DIRS=os.path.join(BASE_DIR,'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
