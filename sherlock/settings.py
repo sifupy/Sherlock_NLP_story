9a72a6c1c0c5b7879e09df019301f01414355f8e
sifupy <sifeddine.addar@gmail.com>
2025-02-04 16:02:24 +0100

    v1.3

--- sherlock/settings.py
+++ sherlock/settings.py
@@ -124,9 +124,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+
+import os
+
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'theme/static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 
 # Default primary key field type
